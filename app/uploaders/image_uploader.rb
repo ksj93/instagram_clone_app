11af799dee311cb861ad614a209ac741b24ab0e8
kim sungjoo <ksjee17@gmail.com>
2021-10-17 14:21:53 +0900

    add_function:user_profile_image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,5 +5,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  # version :thumb do
+  #   process :resize_to_fill => [width, height, gravity = ::Magick::CenterGravity]
+  # end
+  def auto
+    manipulate! do|image|
+      image.auto_orient
+    end
+  end
 
+  process :auto
 end
