9dbd99f6f34201a2824f545358071717b9e40f3f
kim sungjoo <ksjee17@gmail.com>
2021-10-17 20:14:38 +0900

    add seed_date & modificationlocale_settiong

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ module DiveIntoWork
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    # config.i18n.default_locale = :ja
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.generators do |g|
